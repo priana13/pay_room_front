f137df2c2306437bf168e4bf4f0148644371086c
priana13 <herian.sap@gmail.com>
2023-02-21 06:36:04 +0700

    temp booking talbe

---
+++ database/migrations/2023_02_20_233131_create_tem_booking_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temp_booking', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->string('warga_negara');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('room');
+            $table->integer('guest');
+            $table->integer('hotel_id');
+            $table->string('slug');
+            $table->string('transaction_code')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tem_booking');
+    }
+};
